f4870d3bec18a7426b01d7c44bb3fcbc857f628a
Phrase <support@phrase.com>
2020-07-17 14:06:46 +0000

    Deploying from  phrase/openapi@9b61d553

--- cmd/api_translations.go
+++ cmd/api_translations.go
@@ -612,7 +612,7 @@ func initTranslationsByKey() {
 	AddFlag(TranslationsByKey, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
 	AddFlag(TranslationsByKey, "string", helpers.ToSnakeCase("Sort"), "", "Sort criteria. Can be one of: key_name, created_at, updated_at.", false)
 	AddFlag(TranslationsByKey, "string", helpers.ToSnakeCase("Order"), "", "Order direction. Can be one of: asc, desc.", false)
-	AddFlag(TranslationsByKey, "string", helpers.ToSnakeCase("Q"), "", "q_description_placeholder", false)
+	AddFlag(TranslationsByKey, "string", helpers.ToSnakeCase("Q"), "", "Specify a query to find translations by content (including wildcards).<br><br> The following qualifiers are supported in the query:<br> <ul>   <li><code>id:translation_id,...</code> for queries on a comma-separated list of ids</li>   <li><code>unverified:{true|false}</code> for verification status</li>   <li><code>tags:XYZ</code> for tags on the translation</li>   <li><code>excluded:{true|false}</code> for exclusion status</li>   <li><code>updated_at:{>=|<=}2013-02-21T00:00:00Z</code> for date range queries</li> </ul> Find more examples <a href=\"#overview--usage-examples\">here</a>. ", false)
 
 	params.BindPFlags(TranslationsByKey.Flags())
 }
@@ -691,7 +691,7 @@ func initTranslationsByLocale() {
 	AddFlag(TranslationsByLocale, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
 	AddFlag(TranslationsByLocale, "string", helpers.ToSnakeCase("Sort"), "", "Sort criteria. Can be one of: key_name, created_at, updated_at.", false)
 	AddFlag(TranslationsByLocale, "string", helpers.ToSnakeCase("Order"), "", "Order direction. Can be one of: asc, desc.", false)
-	AddFlag(TranslationsByLocale, "string", helpers.ToSnakeCase("Q"), "", "q_description_placeholder", false)
+	AddFlag(TranslationsByLocale, "string", helpers.ToSnakeCase("Q"), "", "Specify a query to find translations by content (including wildcards).<br><br> The following qualifiers are supported in the query:<br> <ul>   <li><code>id:translation_id,...</code> for queries on a comma-separated list of ids</li>   <li><code>unverified:{true|false}</code> for verification status</li>   <li><code>tags:XYZ</code> for tags on the translation</li>   <li><code>excluded:{true|false}</code> for exclusion status</li>   <li><code>updated_at:{>=|<=}2013-02-21T00:00:00Z</code> for date range queries</li> </ul> Find more examples <a href=\"#overview--usage-examples\">here</a>. ", false)
 
 	params.BindPFlags(TranslationsByLocale.Flags())
 }
@@ -890,7 +890,7 @@ func initTranslationsList() {
 	AddFlag(TranslationsList, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
 	AddFlag(TranslationsList, "string", helpers.ToSnakeCase("Sort"), "", "Sort criteria. Can be one of: key_name, created_at, updated_at.", false)
 	AddFlag(TranslationsList, "string", helpers.ToSnakeCase("Order"), "", "Order direction. Can be one of: asc, desc.", false)
-	AddFlag(TranslationsList, "string", helpers.ToSnakeCase("Q"), "", "q_description_placeholder", false)
+	AddFlag(TranslationsList, "string", helpers.ToSnakeCase("Q"), "", "Specify a query to find translations by content (including wildcards).<br><br> The following qualifiers are supported in the query:<br> <ul>   <li><code>id:translation_id,...</code> for queries on a comma-separated list of ids</li>   <li><code>tags:XYZ</code> for tags on the translation</li>   <li><code>unverified:{true|false}</code> for verification status</li>   <li><code>excluded:{true|false}</code> for exclusion status</li>   <li><code>updated_at:{>=|<=}2013-02-21T00:00:00Z</code> for date range queries</li> </ul> Find more examples <a href=\"#overview--usage-examples\">here</a>. ", false)
 
 	params.BindPFlags(TranslationsList.Flags())
 }
