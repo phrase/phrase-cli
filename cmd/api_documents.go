8269617766da7b59fafa4a1a15592790a4c048a2
Phrase <support@phrase.com>
2020-09-23 15:07:23 +0000

    Deploying from  phrase/openapi@0e880351

---
+++ cmd/api_documents.go
@@ -0,0 +1,83 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/antihax/optional"
+	helpers "github.com/phrase/phrase-cli/helpers"
+	api "github.com/phrase/phrase-go"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	initDocumentsList()
+
+	rootCmd.AddCommand(DocumentsApiCmd)
+}
+
+var DocumentsApiCmd = &cobra.Command{
+	Use:   helpers.ToSnakeCase("Documents"),
+	Short: "Documents API",
+}
+
+func initDocumentsList() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("documents/list", "/")[1:], "_")
+	var DocumentsList = &cobra.Command{
+		Use:   use,
+		Short: "List documents",
+		Long:  `List all documents the current user has access to.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.DocumentsListOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+			if params.IsSet(helpers.ToSnakeCase("page")) {
+				localVarOptionals.Page = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("Page")))
+			}
+			if params.IsSet(helpers.ToSnakeCase("perPage")) {
+				localVarOptionals.PerPage = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("PerPage")))
+			}
+
+			data, api_response, err := client.DocumentsApi.DocumentsList(auth, &localVarOptionals)
+
+			if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+
+				fmt.Printf("%s\n", string(jsonBuf))
+			}
+			if err != nil {
+				HandleError(err)
+			}
+
+			if Config.Debug {
+				fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+			}
+		},
+	}
+
+	DocumentsApiCmd.AddCommand(DocumentsList)
+	AddFlag(DocumentsList, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(DocumentsList, "int32", helpers.ToSnakeCase("Page"), "", "Page number", false)
+	AddFlag(DocumentsList, "int32", helpers.ToSnakeCase("PerPage"), "", "allows you to specify a page size up to 100 items, 25 by default", false)
+
+	params.BindPFlags(DocumentsList.Flags())
+}
