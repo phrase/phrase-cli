d2609d64341ffede8cb6cc60b2516fb0cca7df83
Phrase <support@phrase.com>
2020-09-24 11:10:33 +0000

    Deploying from  phrase/openapi@61938f07

---
+++ cmd/api_git_hub_sync.go
@@ -0,0 +1,79 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/antihax/optional"
+	helpers "github.com/phrase/phrase-cli/helpers"
+	api "github.com/phrase/phrase-go"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	initGithubSyncExport()
+
+	rootCmd.AddCommand(GitHubSyncApiCmd)
+}
+
+var GitHubSyncApiCmd = &cobra.Command{
+	Use:   helpers.ToSnakeCase("GitHubSync"),
+	Short: "GitHubSync API",
+}
+
+func initGithubSyncExport() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("github_sync/export", "/")[1:], "_")
+	var GithubSyncExport = &cobra.Command{
+		Use:   use,
+		Short: "Export from Phrase to GitHub",
+		Long:  `Export translations from Phrase to GitHub according to the .phraseapp.yml file within the GitHub repository.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.GithubSyncExportOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			githubSyncExportParameters := api.GithubSyncExportParameters{}
+			if err := json.Unmarshal([]byte(params.GetString("data")), &githubSyncExportParameters); err != nil {
+				HandleError(err)
+			}
+			if Config.Debug {
+				fmt.Printf("%+v\n", githubSyncExportParameters)
+			}
+			data, api_response, err := client.GitHubSyncApi.GithubSyncExport(auth, githubSyncExportParameters, &localVarOptionals)
+
+			if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				os.Stdout.Write(data)
+			}
+			if err != nil {
+				HandleError(err)
+			}
+
+			if Config.Debug {
+				fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+			}
+		},
+	}
+
+	GitHubSyncApiCmd.AddCommand(GithubSyncExport)
+	AddFlag(GithubSyncExport, "string", "data", "d", "payload in JSON format", true)
+	AddFlag(GithubSyncExport, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+
+	params.BindPFlags(GithubSyncExport.Flags())
+}
