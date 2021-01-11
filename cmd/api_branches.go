5037030d9d3f1ed3efc07a209d7fc7537074eafe
Phrase <support@phrase.com>
2021-01-11 16:03:24 +0000

    Deploying from  phrase/openapi@70b04778

--- cmd/api_branches.go
+++ cmd/api_branches.go
@@ -21,7 +21,6 @@ func init() {
 	initBranchShow()
 	initBranchUpdate()
 	initBranchesList()
-	initVariablesList()
 
 	rootCmd.AddCommand(BranchesApiCmd)
 }
@@ -481,73 +480,4 @@ func initBranchesList() {
 	AddFlag(BranchesList, "int32", helpers.ToSnakeCase("PerPage"), "", "allows you to specify a page size up to 100 items, 25 by default", false)
 
 	params.BindPFlags(BranchesList.Flags())
-}
-func initVariablesList() {
-	params := viper.New()
-	var use string
-	// this weird approach is due to mustache template limitations
-	use = strings.Join(strings.Split("variables/list", "/")[1:], "_")
-	var VariablesList = &cobra.Command{
-		Use:   use,
-		Short: "List variables",
-		Long:  `List all variables for the current project.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			auth := Auth()
-
-			cfg := api.NewConfiguration()
-			cfg.SetUserAgent(Config.UserAgent)
-			client := api.NewAPIClient(cfg)
-			localVarOptionals := api.VariablesListOpts{}
-
-			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
-				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
-			}
-
-			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
-				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
-			}
-			if params.IsSet(helpers.ToSnakeCase("page")) {
-				localVarOptionals.Page = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("Page")))
-			}
-			if params.IsSet(helpers.ToSnakeCase("perPage")) {
-				localVarOptionals.PerPage = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("PerPage")))
-			}
-
-			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
-
-			data, api_response, err := client.BranchesApi.VariablesList(auth, projectId, &localVarOptionals)
-
-			if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
-				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
-				if jsonErr != nil {
-					fmt.Printf("%v\n", data)
-					HandleError(err)
-				}
-
-				fmt.Printf("%s\n", string(jsonBuf))
-			}
-			if err != nil {
-				switch castedError := err.(type) {
-				case api.GenericOpenAPIError:
-					fmt.Printf("\n%s\n\n", string(castedError.Body()))
-					HandleError(castedError)
-
-				default:
-					HandleError(castedError)
-				}
-			}
-
-			if Config.Debug {
-				fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
-			}
-		},
-	}
-
-	BranchesApiCmd.AddCommand(VariablesList)
-	AddFlag(VariablesList, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
-	AddFlag(VariablesList, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
-	AddFlag(VariablesList, "int32", helpers.ToSnakeCase("Page"), "", "Page number", false)
-	AddFlag(VariablesList, "int32", helpers.ToSnakeCase("PerPage"), "", "allows you to specify a page size up to 100 items, 25 by default", false)
-
-	params.BindPFlags(VariablesList.Flags())
 }
