e47405fc417f0a0471304738cac11d7e71a5d49b
Phrase <support@phrase.com>
2021-06-15 06:59:41 +0000

    Deploying from  phrase/openapi@32eefe8f

--- cmd/api_locales.go
+++ cmd/api_locales.go
@@ -15,6 +15,7 @@ import (
 )
 
 func init() {
+	initAccountLocales()
 	initLocaleCreate()
 	initLocaleDelete()
 	initLocaleDownload()
@@ -30,6 +31,73 @@ var LocalesApiCmd = &cobra.Command{
 	Short: "Locales API",
 }
 
+func initAccountLocales() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("account/locales", "/")[1:], "_")
+	var AccountLocales = &cobra.Command{
+		Use:   use,
+		Short: "List locales used in account",
+		Long:  `List all locales unique by locale code used across all projects within an account.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.AccountLocalesOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+			if params.IsSet(helpers.ToSnakeCase("page")) {
+				localVarOptionals.Page = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("Page")))
+			}
+			if params.IsSet(helpers.ToSnakeCase("perPage")) {
+				localVarOptionals.PerPage = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("PerPage")))
+			}
+
+			data, api_response, err := client.LocalesApi.AccountLocales(auth, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	LocalesApiCmd.AddCommand(AccountLocales)
+	AddFlag(AccountLocales, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(AccountLocales, "int32", helpers.ToSnakeCase("Page"), "", "Page number", false)
+	AddFlag(AccountLocales, "int32", helpers.ToSnakeCase("PerPage"), "", "allows you to specify a page size up to 100 items, 25 by default", false)
+
+	params.BindPFlags(AccountLocales.Flags())
+}
 func initLocaleCreate() {
 	params := viper.New()
 	var use string
