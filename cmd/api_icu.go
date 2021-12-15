4d43b9061e8a43dcabd5fb367081c0f759733344
Phrase <support@phrase.com>
2021-12-15 08:24:10 +0000

    Deploying from  phrase/openapi@e85585b5

---
+++ cmd/api_icu.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/antihax/optional"
+	helpers "github.com/phrase/phrase-cli/helpers"
+	api "github.com/phrase/phrase-go/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	initIcuSkeleton()
+
+	rootCmd.AddCommand(ICUApiCmd)
+}
+
+var ICUApiCmd = &cobra.Command{
+	Use:   helpers.ToSnakeCase("ICU"),
+	Short: "ICU API",
+}
+
+func initIcuSkeleton() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("icu/skeleton", "/")[1:], "_")
+	var IcuSkeleton = &cobra.Command{
+		Use:   use,
+		Short: "Build icu skeletons",
+		Long:  `Returns icu skeletons for multiple locale codes based on a source content.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.IcuSkeletonOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			icuSkeletonParameters := api.IcuSkeletonParameters{}
+			if err := json.Unmarshal([]byte(params.GetString("data")), &icuSkeletonParameters); err != nil {
+				HandleError(err)
+			}
+			if Config.Debug {
+				fmt.Printf("%+v\n", icuSkeletonParameters)
+			}
+			data, api_response, err := client.ICUApi.IcuSkeleton(auth, icuSkeletonParameters, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	ICUApiCmd.AddCommand(IcuSkeleton)
+	AddFlag(IcuSkeleton, "string", "data", "d", "payload in JSON format", true)
+	AddFlag(IcuSkeleton, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+
+	params.BindPFlags(IcuSkeleton.Flags())
+}
