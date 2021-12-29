743050fc33f3679ba8b4a2f1c81f781f2f90598a
Phrase <support@phrase.com>
2021-12-29 13:53:58 +0000

    Deploying from  phrase/openapi@aa6b225f

--- build/release.sh
+++ build/release.sh
@@ -36,9 +36,9 @@ EOF
 }
 
 echo "Create release $VERSION"
-api_url="https://api.github.com/repos/phrase/phrase-cli/releases?access_token=${GITHUB_TOKEN}"
-response="$(curl --data "$(create_release_data)" ${api_url})"
-release_id=$(echo $response | python -c "import sys, json; print(json.load(sys.stdin)['id'])")
+api_url="https://api.github.com/repos/phrase/phrase-cli/releases"
+response="$(curl -H "Authorization: token ${GITHUB_TOKEN}" --data "$(create_release_data)" ${api_url})"
+release_id=$(echo $response | python -c "import sys, json; print(json.load(sys.stdin).get('id', ''))")
 
 if [ -z "$release_id" ]
 then
@@ -53,8 +53,8 @@ fi
 DIST_DIR="./dist"
 for file in "$DIST_DIR"/*; do
   echo "Uploading ${file}"
-  asset="https://uploads.github.com/repos/phrase/phrase-cli/releases/${release_id}/assets?name=$(basename "$file")&access_token=${GITHUB_TOKEN}"
-  curl -sS --data-binary @"$file" -H "Content-Type: application/octet-stream" $asset > /dev/null
+  asset="https://uploads.github.com/repos/phrase/phrase-cli/releases/${release_id}/assets?name=$(basename "$file")"
+  curl -sS --data-binary @"$file" -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: application/octet-stream" $asset > /dev/null
   echo Hash: $(sha256sum $file)
 done
 
