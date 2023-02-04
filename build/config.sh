af8c6ceb6237aff39c8f906e35c66ceae1452442
Phrase <support@phrase.com>
2023-02-04 11:13:59 +0000

    Deploying from  phrase/openapi@a53cfaa1

--- build/config.sh
+++ build/config.sh
@@ -4,7 +4,7 @@ set -e
 wd=$(realpath $(dirname $0)/..)
 export BUILD_DIR=$(realpath $(dirname $0)/..)
 pushd $BUILD_DIR > /dev/null
-export GOVERSION=${GOVERSION:-1.16}
+export GOVERSION=${GOVERSION:-1.19}
 export REVISION=${GIT_COMMIT:-$(git rev-parse HEAD)}
 export LIBRARY_REVISION=$(cat go.sum | grep github.com/phrase/phrase-go | tail -n 1 | cut -d " " -f 2 | cut -d "-" -f 3 | cut -d "+" -f 1)
 export SOURCE_DATE_EPOCH=$(git log -1 --format=%ct)
