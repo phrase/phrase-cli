ab7c5eeacd47dbe59d122fcd8973d2e1da46b6d7
Phrase <support@phrase.com>
2023-10-13 07:48:41 +0000

    Deploying from  phrase/openapi@970e612f

---
+++ cmd/api_figma_attachments.go
@@ -0,0 +1,392 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/antihax/optional"
+	helpers "github.com/phrase/phrase-cli/helpers"
+	api "github.com/phrase/phrase-go/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	initFigmaAttachmentCreate()
+	initFigmaAttachmentDelete()
+	initFigmaAttachmentShow()
+	initFigmaAttachmentUpdate()
+	initFigmaAttachmentsList()
+
+	rootCmd.AddCommand(FigmaAttachmentsApiCmd)
+}
+
+var FigmaAttachmentsApiCmd = &cobra.Command{
+	Use:   helpers.ToSnakeCase("FigmaAttachments"),
+	Short: "FigmaAttachments API",
+}
+
+func initFigmaAttachmentCreate() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachment/create", "/")[1:], "_")
+	var FigmaAttachmentCreate = &cobra.Command{
+		Use:   use,
+		Short: "Create a Figma attachment",
+		Long:  `Create a new Figma attachment.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentCreateOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			figmaAttachmentCreateParameters := api.FigmaAttachmentCreateParameters{}
+			if err := json.Unmarshal([]byte(params.GetString("data")), &figmaAttachmentCreateParameters); err != nil {
+				HandleError(err)
+			}
+			if Config.Debug {
+				fmt.Printf("%+v\n", figmaAttachmentCreateParameters)
+			}
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.FigmaAttachmentsApi.FigmaAttachmentCreate(auth, projectId, figmaAttachmentCreateParameters, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	FigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentCreate)
+	AddFlag(FigmaAttachmentCreate, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentCreate, "string", "data", "d", "payload in JSON format", true)
+	AddFlag(FigmaAttachmentCreate, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentCreate, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentCreate.Flags())
+}
+func initFigmaAttachmentDelete() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachment/delete", "/")[1:], "_")
+	var FigmaAttachmentDelete = &cobra.Command{
+		Use:   use,
+		Short: "Delete a Figma attachment",
+		Long:  `Delete an existing Figma attachment.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentDeleteOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			id := params.GetString(helpers.ToSnakeCase("Id"))
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.FigmaAttachmentsApi.FigmaAttachmentDelete(auth, projectId, id, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				os.Stdout.Write(data)
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	FigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentDelete)
+	AddFlag(FigmaAttachmentDelete, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentDelete, "string", helpers.ToSnakeCase("Id"), "", "ID", true)
+	AddFlag(FigmaAttachmentDelete, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentDelete, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentDelete.Flags())
+}
+func initFigmaAttachmentShow() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachment/show", "/")[1:], "_")
+	var FigmaAttachmentShow = &cobra.Command{
+		Use:   use,
+		Short: "Get a single Figma attachment",
+		Long:  `Get details on a single Figma attachment for a given project.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentShowOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			id := params.GetString(helpers.ToSnakeCase("Id"))
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.FigmaAttachmentsApi.FigmaAttachmentShow(auth, projectId, id, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	FigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentShow)
+	AddFlag(FigmaAttachmentShow, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentShow, "string", helpers.ToSnakeCase("Id"), "", "ID", true)
+	AddFlag(FigmaAttachmentShow, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentShow, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentShow.Flags())
+}
+func initFigmaAttachmentUpdate() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachment/update", "/")[1:], "_")
+	var FigmaAttachmentUpdate = &cobra.Command{
+		Use:   use,
+		Short: "Update a Figma attachment",
+		Long:  `Update an existing Figma attachment.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentUpdateOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			id := params.GetString(helpers.ToSnakeCase("Id"))
+			figmaAttachmentUpdateParameters := api.FigmaAttachmentUpdateParameters{}
+			if err := json.Unmarshal([]byte(params.GetString("data")), &figmaAttachmentUpdateParameters); err != nil {
+				HandleError(err)
+			}
+			if Config.Debug {
+				fmt.Printf("%+v\n", figmaAttachmentUpdateParameters)
+			}
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.FigmaAttachmentsApi.FigmaAttachmentUpdate(auth, projectId, id, figmaAttachmentUpdateParameters, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	FigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentUpdate)
+	AddFlag(FigmaAttachmentUpdate, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentUpdate, "string", helpers.ToSnakeCase("Id"), "", "ID", true)
+	AddFlag(FigmaAttachmentUpdate, "string", "data", "d", "payload in JSON format", true)
+	AddFlag(FigmaAttachmentUpdate, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentUpdate, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentUpdate.Flags())
+}
+func initFigmaAttachmentsList() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachments/list", "/")[1:], "_")
+	var FigmaAttachmentsList = &cobra.Command{
+		Use:   use,
+		Short: "List Figma attachments",
+		Long:  `List all Figma attachments for the given project`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentsListOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("page")) {
+				localVarOptionals.Page = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("Page")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("perPage")) {
+				localVarOptionals.PerPage = optional.NewInt32(params.GetInt32(helpers.ToSnakeCase("PerPage")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.FigmaAttachmentsApi.FigmaAttachmentsList(auth, projectId, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	FigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentsList)
+	AddFlag(FigmaAttachmentsList, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentsList, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentsList, "int32", helpers.ToSnakeCase("Page"), "", "Page number", false)
+	AddFlag(FigmaAttachmentsList, "int32", helpers.ToSnakeCase("PerPage"), "", "Limit on the number of objects to be returned, between 1 and 100. 25 by default", false)
+	AddFlag(FigmaAttachmentsList, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentsList.Flags())
+}
---
+++ cmd/api_keys_figma_attachments.go
@@ -0,0 +1,158 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/antihax/optional"
+	helpers "github.com/phrase/phrase-cli/helpers"
+	api "github.com/phrase/phrase-go/v2"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	initFigmaAttachmentAttachToKey()
+	initFigmaAttachmentDetachFromKey()
+
+	rootCmd.AddCommand(KeysFigmaAttachmentsApiCmd)
+}
+
+var KeysFigmaAttachmentsApiCmd = &cobra.Command{
+	Use:   helpers.ToSnakeCase("KeysFigmaAttachments"),
+	Short: "KeysFigmaAttachments API",
+}
+
+func initFigmaAttachmentAttachToKey() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachment/attach_to_key", "/")[1:], "_")
+	var FigmaAttachmentAttachToKey = &cobra.Command{
+		Use:   use,
+		Short: "Attach the Figma attachment to a key",
+		Long:  `Attach the Figma attachment to a key`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentAttachToKeyOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			figmaAttachmentId := params.GetString(helpers.ToSnakeCase("FigmaAttachmentId"))
+			id := params.GetString(helpers.ToSnakeCase("Id"))
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.KeysFigmaAttachmentsApi.FigmaAttachmentAttachToKey(auth, projectId, figmaAttachmentId, id, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				os.Stdout.Write(data)
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	KeysFigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentAttachToKey)
+	AddFlag(FigmaAttachmentAttachToKey, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentAttachToKey, "string", helpers.ToSnakeCase("FigmaAttachmentId"), "", "Figma attachment ID", true)
+	AddFlag(FigmaAttachmentAttachToKey, "string", helpers.ToSnakeCase("Id"), "", "ID", true)
+	AddFlag(FigmaAttachmentAttachToKey, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentAttachToKey, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentAttachToKey.Flags())
+}
+func initFigmaAttachmentDetachFromKey() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("figma_attachment/detach_from_key", "/")[1:], "_")
+	var FigmaAttachmentDetachFromKey = &cobra.Command{
+		Use:   use,
+		Short: "Detach the Figma attachment from a key",
+		Long:  `Detach the Figma attachment from a key`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.FigmaAttachmentDetachFromKeyOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			projectId := params.GetString(helpers.ToSnakeCase("ProjectId"))
+			figmaAttachmentId := params.GetString(helpers.ToSnakeCase("FigmaAttachmentId"))
+			id := params.GetString(helpers.ToSnakeCase("Id"))
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			if params.IsSet(helpers.ToSnakeCase("branch")) {
+				localVarOptionals.Branch = optional.NewString(params.GetString(helpers.ToSnakeCase("Branch")))
+			}
+
+			data, api_response, err := client.KeysFigmaAttachmentsApi.FigmaAttachmentDetachFromKey(auth, projectId, figmaAttachmentId, id, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				os.Stdout.Write(data)
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	KeysFigmaAttachmentsApiCmd.AddCommand(FigmaAttachmentDetachFromKey)
+	AddFlag(FigmaAttachmentDetachFromKey, "string", helpers.ToSnakeCase("ProjectId"), "", "Project ID", true)
+	AddFlag(FigmaAttachmentDetachFromKey, "string", helpers.ToSnakeCase("FigmaAttachmentId"), "", "Figma attachment ID", true)
+	AddFlag(FigmaAttachmentDetachFromKey, "string", helpers.ToSnakeCase("Id"), "", "ID", true)
+	AddFlag(FigmaAttachmentDetachFromKey, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+	AddFlag(FigmaAttachmentDetachFromKey, "string", helpers.ToSnakeCase("Branch"), "", "specify the branch to use", false)
+
+	params.BindPFlags(FigmaAttachmentDetachFromKey.Flags())
+}
