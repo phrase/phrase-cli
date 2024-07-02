ffcb69932cef5bb19069b10f8f046c9c65877904
Phrase <support@phrase.com>
2024-07-02 12:13:25 +0000

    Deploying from  phrase/openapi@e1d9cfb2

--- cmd/api_repo_syncs.go
+++ cmd/api_repo_syncs.go
@@ -15,6 +15,7 @@ import (
 func init() {
 	initRepoSyncActivate()
 	initRepoSyncDeactivate()
+	initRepoSyncEventShow()
 	initRepoSyncEvents()
 	initRepoSyncExport()
 	initRepoSyncImport()
@@ -160,6 +161,75 @@ func initRepoSyncDeactivate() {
 	AddFlag(RepoSyncDeactivate, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
 
 	params.BindPFlags(RepoSyncDeactivate.Flags())
+}
+func initRepoSyncEventShow() {
+	params := viper.New()
+	var use string
+	// this weird approach is due to mustache template limitations
+	use = strings.Join(strings.Split("repo_sync_event/show", "/")[1:], "_")
+	var RepoSyncEventShow = &cobra.Command{
+		Use:   use,
+		Short: "Get a single Repo Sync Event",
+		Long:  `Shows a single Repo Sync event.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			auth := Auth()
+
+			cfg := api.NewConfiguration()
+			cfg.SetUserAgent(Config.UserAgent)
+			if Config.Credentials.Host != "" {
+				cfg.BasePath = Config.Credentials.Host
+			}
+
+			client := api.NewAPIClient(cfg)
+			localVarOptionals := api.RepoSyncEventShowOpts{}
+
+			if Config.Credentials.TFA && Config.Credentials.TFAToken != "" {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(Config.Credentials.TFAToken)
+			}
+
+			accountId := params.GetString(helpers.ToSnakeCase("AccountId"))
+
+			repoSyncId := params.GetString(helpers.ToSnakeCase("RepoSyncId"))
+
+			id := params.GetString(helpers.ToSnakeCase("Id"))
+
+			if params.IsSet(helpers.ToSnakeCase("xPhraseAppOTP")) {
+				localVarOptionals.XPhraseAppOTP = optional.NewString(params.GetString(helpers.ToSnakeCase("XPhraseAppOTP")))
+			}
+
+			data, api_response, err := client.RepoSyncsApi.RepoSyncEventShow(auth, accountId, repoSyncId, id, &localVarOptionals)
+
+			if err != nil {
+				switch castedError := err.(type) {
+				case api.GenericOpenAPIError:
+					fmt.Printf("\n%s\n\n", string(castedError.Body()))
+					HandleError(castedError)
+
+				default:
+					HandleError(castedError)
+				}
+			} else if api_response.StatusCode >= 200 && api_response.StatusCode < 300 {
+				jsonBuf, jsonErr := json.MarshalIndent(data, "", " ")
+				if jsonErr != nil {
+					fmt.Printf("%v\n", data)
+					HandleError(err)
+				}
+				fmt.Printf("%s\n", string(jsonBuf))
+
+				if Config.Debug {
+					fmt.Printf("%+v\n", api_response) // &{Response:0xc00011ccf0 NextPage:2 FirstPage:1 LastPage:4 Rate:{Limit:1000 Remaining:998 Reset:2020-04-25 00:35:00 +0200 CEST}}
+				}
+			}
+		},
+	}
+
+	RepoSyncsApiCmd.AddCommand(RepoSyncEventShow)
+	AddFlag(RepoSyncEventShow, "string", helpers.ToSnakeCase("AccountId"), "", "Account ID", true)
+	AddFlag(RepoSyncEventShow, "string", helpers.ToSnakeCase("RepoSyncId"), "", "Repo Sync ID", true)
+	AddFlag(RepoSyncEventShow, "string", helpers.ToSnakeCase("Id"), "", "ID", true)
+	AddFlag(RepoSyncEventShow, "string", helpers.ToSnakeCase("XPhraseAppOTP"), "", "Two-Factor-Authentication token (optional)", false)
+
+	params.BindPFlags(RepoSyncEventShow.Flags())
 }
 func initRepoSyncEvents() {
 	params := viper.New()
